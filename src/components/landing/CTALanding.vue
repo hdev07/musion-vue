789caa266872e98334ef439ac9bb6a4ddae8b4a6
Alejandro Cruz <hcruz0716@gmail.com>
2023-05-26 16:44:00 -0600

    Add TEXT

--- src/components/landing/CTALanding.vue
+++ src/components/landing/CTALanding.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts"></script>
-
 <template>
   <section class="bg-background text-white">
     <div class="mx-auto max-w-screen-lg px-4 py-32 h-screen flex items-center">
@@ -7,20 +6,21 @@
         <h1
           class="bg-gradient-to-b from-[#F48FB1] to-[#FF4081] bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
         >
-          Lorem ipsum dolor sit amet
-          <span class="sm:block pb-2"> adipisicing elit. Nesciunt </span>
+          Explora la cultura sin limites
+          <span class="sm:block pb-2"> </span>
         </h1>
         <p class="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga
-          ducimus numquam ea!
+          Explora los museos más fascinantes de la ciudad y sumérgete en su historia y arte.
         </p>
         <div class="mt-8 flex flex-wrap justify-center gap-4">
           <a
+            href="'https://app.musion.day'"
             class="block w-full rounded border border-primary px-12 py-3 text-sm font-medium text-white hover:bg-primary focus:outline-none focus:ring active:bg-primary sm:w-auto"
           >
             Registrate
           </a>
           <a
+            href="'https://app.musion.day/login'"
             class="block w-full rounded border border-primary bg-primary px-12 py-3 text-sm font-medium text-white hover:shadow-xl focus:outline-none focus:ring active:text-opacity-75 sm:w-auto hover:bg-transparent hover:text-white hover:shadow-pink-500/30"
           >
             Inicia sesion
