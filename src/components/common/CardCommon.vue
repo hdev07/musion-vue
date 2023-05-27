beefd1be4ca44b0eeb0ff1298f3e1bcdc18263ac
Alejandro Cruz <hcruz0716@gmail.com>
2023-05-27 16:22:35 -0600

    Add testimonials

--- src/components/common/CardCommon.vue
+++ src/components/common/CardCommon.vue
@@ -3,12 +3,14 @@ const props = defineProps({
   title: { type: String, default: '' },
   data: { type: String, default: '' },
   sm: { type: String, default: '' },
-  icon: { type: String, default: '' }
+  icon: { type: String, default: '' },
+  img: { type: String, default: '' },
+  stars: { type: String, default: '' }
 })
 </script>
 <template>
   <div
-    v-if="props.icon === ''"
+    v-if="props.icon === '' && props.stars === ''"
     class="block rounded-xl border border-pink-500/10 p-8 transition bg-secondary hover:shadow-xl hover:border-pink-500/30 hover:shadow-pink-500/30"
   >
     <div class="text-xl font-bold text-white">{{ props?.title }}</div>
@@ -25,10 +27,26 @@ const props = defineProps({
     <h2 class="mt-4 text-xl font-bold text-white">
       {{ props?.title }}
     </h2>
-
     <p class="mt-1 text-sm text-gray-300">
       {{ props?.data }}
     </p>
   </div>
-  <div v-else></div>
+  <div
+    v-else-if="props.stars"
+    class="block rounded-xl border border-pink-500/10 p-8 transition bg-secondary hover:shadow-xl hover:border-pink-500/30 hover:shadow-pink-500/30"
+  >
+    <div class="flex items-center gap-4">
+      <img :src="props?.img" alt="Man" class="h-16 w-16 rounded-full object-cover" />
+      <div>
+        <div class="flex justify-start gap-0.5 text-primary">
+          <vue-feather v-for="index in 5" :key="index" type="star" fill="#FF4081" size="1rem" />
+          <!-- <vue-feather type="star" stroke="#FF4081" size="1rem" fill="none" /> -->
+        </div>
+        <p class="mt-1 text-lg font-medium text-white">{{ props?.title }}</p>
+      </div>
+    </div>
+    <p class="mt-4 text-white">
+      {{ props?.data }}
+    </p>
+  </div>
 </template>
--- src/components/landing/TestimonialsLanding.vue
+++ src/components/landing/TestimonialsLanding.vue
@@ -1,114 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import TitleCommon from '@/components/common/TitleCommon.vue'
+import CardCommon from '@/components/common/CardCommon.vue'
+
+const title = ref('Nuestros usuarios')
+const description = ref('Que opinan nuestros usuarios de musion')
+const Comments = ref([
+  {
+    img: 'https://i.pravatar.cc/300',
+    name: 'María G.',
+    comment:
+      '¡Musion es la herramienta perfecta para los amantes del arte y la cultura! Me encanta cómo puedo explorar todos los museos de la Ciudad de México en un solo lugar. Además, las rutas personalizadas me ayudan a planificar mis visitas de manera eficiente. ¡Definitivamente lo recomiendo!',
+    stars: '5'
+  },
+  {
+    img: 'https://i.pravatar.cc/301',
+    name: 'Juan C.',
+    comment:
+      'Descubrí Musion y ahora no puedo visitar un museo sin consultarlo primero. Me gusta cómo puedo ver las exposiciones actuales y los eventos especiales en cada museo. Además, la función de comentarios me permite conocer las opiniones de otros usuarios. ¡Una aplicación imprescindible para los aficionados a los museos!"',
+    stars: '5'
+  },
+  {
+    img: 'https://i.pravatar.cc/302',
+    name: 'Alejandra M.',
+    comment:
+      'Como turista, Musion ha sido mi guía confiable para explorar los museos de la Ciudad de México. La información detallada, los horarios de apertura y las indicaciones precisas me han ahorrado mucho tiempo y esfuerzo. ¡Definitivamente recomendaría Musion a todos los visitantes de la ciudad!',
+    stars: '5'
+  }
+])
+</script>
 
 <template>
   <section class="bg-background text-white">
     <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
-      <div class="mx-auto max-w-3xl text-center">
-        <h2 class="text-3xl font-bold text-white sm:text-4xl">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit
-        </h2>
-
-        <p class="mt-4 text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores laborum labore
-          provident impedit esse recusandae facere libero harum sequi.
-        </p>
-      </div>
-
+      <TitleCommon :title="title" :description="description" />
       <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <div
-          class="block rounded-xl border border-pink-500/10 bg-secondary p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/30"
-        >
-          <div class="flex items-center gap-4">
-            <img
-              alt="Man"
-              src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
-              class="h-16 w-16 rounded-full object-cover"
-            />
-
-            <div>
-              <div class="flex justify-center gap-0.5 text-primary">
-                <vue-feather
-                  v-for="index in 4"
-                  :key="index"
-                  type="star"
-                  fill="#FF4081"
-                  size="1rem"
-                />
-                <vue-feather type="star" stroke="#FF4081" size="1rem" fill="none" />
-              </div>
-
-              <p class="mt-1 text-lg font-medium text-white">Paul Starr</p>
-            </div>
-          </div>
-
-          <p class="line-clamp-2 sm:line-clamp-none mt-4 text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatem alias ut
-            provident sapiente repellendus.
-          </p>
-        </div>
-
-        <div
-          class="block rounded-xl border border-pink-500/10 bg-secondary p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/30"
-        >
-          <div class="flex items-center gap-4">
-            <img
-              alt="Man"
-              src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
-              class="h-16 w-16 rounded-full object-cover"
-            />
-
-            <div>
-              <div class="flex justify-center gap-0.5 text-primary">
-                <vue-feather
-                  v-for="index in 5"
-                  :key="index"
-                  type="star"
-                  fill="#FF4081"
-                  size="1rem"
-                />
-              </div>
-
-              <p class="mt-1 text-lg font-medium text-white">Paul Starr</p>
-            </div>
-          </div>
-
-          <p class="line-clamp-2 sm:line-clamp-none mt-4 text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatem alias ut
-            provident sapiente repellendus.
-          </p>
-        </div>
-
-        <div
-          class="block rounded-xl border border-pink-500/10 bg-secondary p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/30"
-        >
-          <div class="flex items-center gap-4">
-            <img
-              alt="Man"
-              src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
-              class="h-16 w-16 rounded-full object-cover"
-            />
-
-            <div>
-              <div class="flex justify-center gap-0.5 text-primary">
-                <vue-feather
-                  v-for="index in 4"
-                  :key="index"
-                  type="star"
-                  fill="#FF4081"
-                  size="1rem"
-                />
-                <vue-feather type="star" stroke="#FF4081" size="1rem" fill="none" />
-              </div>
-
-              <p class="mt-1 text-lg font-medium text-white">Paul Starr</p>
-            </div>
-          </div>
-
-          <p class="line-clamp-2 sm:line-clamp-none mt-4 text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatem alias ut
-            provident sapiente repellendus.
-          </p>
-        </div>
+        <CardCommon
+          v-for="comment in Comments"
+          :key="comment?.name"
+          :img="comment?.img"
+          :stars="comment?.stars"
+          :title="comment?.name"
+          :data="comment?.comment"
+        />
       </div>
     </div>
   </section>
