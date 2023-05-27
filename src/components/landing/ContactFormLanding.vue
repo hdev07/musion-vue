5a0bafd576b20ceac31a2f020e4b51b9c78d2428
Alejandro Cruz <hcruz0716@gmail.com>
2023-05-27 16:43:00 -0600

    Text in contact form

--- src/components/landing/ContactFormLanding.vue
+++ src/components/landing/ContactFormLanding.vue
@@ -1,24 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ButtonCommon from '@/components/common/ButtonCommon.vue'
+</script>
 
 <template>
   <section class="bg-background">
     <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
         <div class="lg:col-span-2 lg:py-12">
-          <p class="max-w-xl text-lg text-white">
-            At the same time, the fact that we are wholly owned and totally independent from
-            manufacturer and other group control gives you confidence that we will only recommend
-            what is right for you.
+          <h2 class="text-3xl font-bold text-primary sm:text-4xl">
+            ¡Tu opinión es importante para nosotros!
+          </h2>
+          <p class="max-w-xl text-lg text-white pt-4">
+            En Musion, valoramos tus comentarios y reseñas sobre tu experiencia en los museos de la
+            Ciudad de México. Nos encantaría saber qué te pareció la aplicación, qué museos has
+            visitado y qué impresiones te dejaron.
           </p>
         </div>
 
         <div class="rounded-lg bg-secondary p-8 shadow-lg lg:col-span-3 lg:p-12">
           <form action="" class="space-y-4">
             <div>
-              <label class="sr-only" for="name">Name</label>
+              <label class="sr-only" for="name">Nombre</label>
               <input
                 class="w-full rounded-lg active:border active:border-primary bg-background p-3 text-sm text-white"
-                placeholder="Name"
+                placeholder="Nombre"
                 type="text"
                 id="name"
               />
@@ -26,20 +31,18 @@
 
             <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
               <div>
-                <label class="sr-only" for="email">Email</label>
                 <input
                   class="w-full rounded-lg active:border active:border-primary p-3 text-sm bg-background text-white"
-                  placeholder="Email address"
+                  placeholder="Correo electronico"
                   type="email"
                   id="email"
                 />
               </div>
 
               <div>
-                <label class="sr-only" for="phone">Phone</label>
                 <input
                   class="w-full rounded-lg active:border active:border-primary p-3 text-sm bg-background text-white"
-                  placeholder="Phone Number"
+                  placeholder="Numero de telefono"
                   type="tel"
                   id="phone"
                 />
@@ -47,22 +50,24 @@
             </div>
 
             <div>
-              <label class="sr-only" for="message">Message</label>
-
               <textarea
                 class="w-full rounded-lg active:border active:border-primary p-3 text-sm bg-background text-white"
-                placeholder="Message"
+                placeholder="Mensaje"
                 rows="8"
                 id="message"
               ></textarea>
             </div>
 
             <div class="mt-4 flex justify-center text-center">
-              <a
-                class="block w-full rounded border border-primary bg-primary px-12 py-3 text-sm font-medium text-white hover:shadow-xl focus:outline-none focus:ring active:text-opacity-75 sm:w-auto hover:bg-transparent hover:text-white hover:shadow-pink-500/30"
-              >
-                Send Coments
-              </a>
+              <div class="w-2/3 sm:w-1/3">
+                <ButtonCommon
+                  :to="'https://app.musion.day/login'"
+                  :text="'Enviar comentarios'"
+                  :bgPrimary="true"
+                  :hBgTransparent="true"
+                  :hShadowPink="true"
+                />
+              </div>
             </div>
           </form>
         </div>
